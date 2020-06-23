c700610f2ad1129dd4bbc60a7fabe87d4494414f
Clarence Ji <dev@clarenceji.net>
2020-06-23 23:29:16 +0100

    Initial Commit

---
+++ Tayrics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tayrics
+//
+//  Created by Clarence Ji on 6/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
